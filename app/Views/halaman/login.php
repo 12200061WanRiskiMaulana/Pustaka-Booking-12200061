f64a2df208c5aab7fdaaa618e05495e095df3918
12200061WanRiskiMaulana <12200061@bsi.ac.id>
2021-09-28 15:18:42 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,14 @@
+html>
+    <head>
+        <title>Perpustakaan Login</title>
+    </head>
+    <body>
+
+        <hl>Selamat datang di Sistem Informasi Perpustakaan</hl>
+        <p>
+            Ini adalah layanan perpustakaan digital masa kini 
+            <a href="<?=base_url('/daftar-member')?>">klik daftar</a>
+        </p>
+
+    </body>
+</html>
